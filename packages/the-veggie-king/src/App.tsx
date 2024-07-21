3409e9bd7980e5c6e41a1909844c122579e9f38c
yuta-takeda <ytakeda@jmdc.co.jp>
2024-07-21 16:41:13 +0900

    add navigation

--- packages/the-veggie-king/src/App.tsx
+++ packages/the-veggie-king/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './assets/icons/logo.svg'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="flex fixed top-0 justify-between items-center py-4 px-12 w-screen shadow-xl">
+        <img src={logo} alt="TheVeggieKingLogo" width={154.5} height={30} />
+        <nav>
+          <span className="mx-2">
+            <a href="#">ABOUT US</a>
+          </span>
+          <span className="mx-2">
+            <a href="#">MENU</a>
+          </span>
+          <span className="mx-2">
+            <a href="#">CONTACT</a>
+          </span>
+        </nav>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
