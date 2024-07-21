9e0062c44480e8afa9ede4e292440514371fbb55
yuta-takeda <ytakeda@jmdc.co.jp>
2024-07-21 18:32:08 +0900

    add hero section

--- packages/the-veggie-king/src/App.tsx
+++ packages/the-veggie-king/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import checkMenuIcon from './assets/icons/check-menu-icon.svg'
 import logo from './assets/icons/logo.svg'
+import heroSecFood from './assets/images/hero-section-food.png'
+import heroSecFoodSlide1 from './assets/images/hero-section-slide-food-1.png'
+import heroSecFoodSlide2 from './assets/images/hero-section-slide-food-2.png'
+import heroSecFoodSlide3 from './assets/images/hero-section-slide-food-3.png'
+import heroSecFoodSlide4 from './assets/images/hero-section-slide-food-4.png'
+
+import { Slideshow } from './components/Slideshow'
 
 const App: React.FC = () => {
   return (
     <div>
-      <header className="flex fixed top-0 justify-between items-center py-4 px-12 w-screen shadow-xl">
+      <header className="flex fixed top-0 justify-between items-center px-24 w-screen shadow-xl h-[72px]">
         <img src={logo} alt="TheVeggieKingLogo" width={154.5} height={30} />
         <nav>
           <span className="mx-2">
@@ -18,6 +26,40 @@ const App: React.FC = () => {
           </span>
         </nav>
       </header>
+      <section>
+        <div className="flex justify-center items-center pl-24 h-[calc(100vh_-_72px)] mt-[72px]">
+          <div className="w-2/3">
+            <h2 className="pb-4 text-6xl font-bold leading-tight">
+              Healthy & Fresh
+              <br />
+              Food For You
+            </h2>
+            <p className="pb-4">
+              Created for lovers of healthy, delicious and
+              <br />
+              non-obvious food.
+            </p>
+            <button className="py-2 px-4 bg-orange-500 rounded-full hover:bg-orange-600">
+              <div className="flex items-center">
+                Check menu
+                <img src={checkMenuIcon} alt={'check menu icon'} className="ml-2" />
+              </div>
+            </button>
+          </div>
+          <div className="w-1/3 h-full bg-green-700"></div>
+        </div>
+        <div className="relative">
+          <div className="absolute left-0 bottom-[-5vh]">
+            <img src={heroSecFood} alt={'hero section food image'} />
+          </div>
+          <div className="absolute right-[40vh] bottom-[12vh] w-[65vh]">
+            <Slideshow
+              images={[heroSecFoodSlide1, heroSecFoodSlide2, heroSecFoodSlide3, heroSecFoodSlide4]}
+              intervalMs={5000}
+            />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
---
+++ packages/the-veggie-king/src/components/Slideshow/Slideshow.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+interface Props {
+  images: string[]
+  intervalMs: number
+}
+
+export const Slideshow: React.FC<Props> = (props) => {
+  const { images, intervalMs } = props
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+    }, intervalMs)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="overflow-hidden relative m-4">
+      <div
+        className="flex duration-700 transition-trasform"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {images.map((image, index) => (
+          <img key={index} src={image} alt={`slide ${index}`} className="object-contain" />
+        ))}
+      </div>
+    </div>
+  )
+}
