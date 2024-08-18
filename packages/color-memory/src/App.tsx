8f0456cd1b861f53acd30c25f0d7de7b97516d66
yuta-takeda <ytakeda@jmdc.co.jp>
2024-08-18 16:32:03 +0900

    add circle component

--- packages/color-memory/src/App.tsx
+++ packages/color-memory/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Circle: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative bg-gray-600 rounded-full w-[500px] h-[500px]">
+      <div className="absolute bg-green-300 rounded-tl-full w-[205px] h-[205px] top-[30px] left-[30px]"></div>
+      <div className="absolute bg-red-500 rounded-tr-full w-[205px] h-[205px] top-[30px] right-[30px]"></div>
+      <div className="absolute bg-amber-300 rounded-bl-full w-[205px] h-[205px] bottom-[30px] left-[30px]"></div>
+      <div className="absolute rounded-br-full bg-sky-300 w-[205px] h-[205px] bottom-[30px] right-[30px]"></div>
+      <div className="absolute bg-gray-600 rounded-full w-[200px] h-[200px] top-[150px] left-[150px]"></div>
     </div>
-  );
+  )
 }
 
-export default App;
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen bg-gray-800">
+      <Circle />
+    </div>
+  )
+}
+
+export default App
